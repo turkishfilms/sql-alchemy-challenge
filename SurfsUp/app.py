4df22b95fc6f28e6f64aedf062a7f476d0ccc094
Reginald Robinson <ddrbnsn@gmail.com>
2023-05-21 10:05:35 -0700

    tidied and cleaned

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -8,12 +8,12 @@ import datetime as dt
 
 app = Flask(__name__)
 
-ly_df = pd.read_csv("last_year.csv")
+last_year_df = pd.read_csv("last_year.csv")
 busy_df = pd.read_csv("busy.csv")
 
 def last_year_to_obj():
-    ly_obj = ly_df.set_index("date")["prcp"].to_dict()
-    return ly_obj    
+    last_year_obj = last_year_df.set_index("date")["prcp"].to_dict()
+    return last_year_obj    
 
 def df_from_start(start):
     session = Session(bind = engine)
@@ -34,9 +34,6 @@ Measurement = base.classes.measurement
 
 @app.route("/")
 def home():
-    print("YOLO CHOLO")
-    # Start at the homepage.
-    # List all the available routes.
     return (
         f"Welcome to the Hawaii API!<br/>"
         f"Available Routes:<br/>"
@@ -48,50 +45,40 @@ def home():
         f"/api/v1.0/<start>/<end>/<br/>"
         )
         
-
 @app.route("/api/v1.0/precipitation")
-def nothome():
+def precipitation():
     # Convert the query results from your precipitation analysis (i.e. retrieve only the last 12 months of data) to a dictionary using date as the key and prcp as the value.
     last_year_prcp =last_year_to_obj()
-    # Return the JSON representation of your dictionary.
     return last_year_prcp
 
 @app.route("/api/v1.0/stations")
-def somthing():
-    # Return a JSON list of stations from the dataset.
-    return jsonify(list(ly_df["station"].unique()))
+def stations():
+    return jsonify(list(last_year_df["station"].unique()))
 
 @app.route("/api/v1.0/tobs")
-def waitasecond():
+def tobs():
     # Query the dates and temperature observations of the most-active station for the previous year of data.
-    
-    # Return a JSON list of temperature observations for the previous year.
     return busy_df.set_index("date")["tobs"].to_dict()
 
 @app.route("/api/v1.0/<start>")
-def noway(start):
+def starting(start):
     start_f = start.replace(" ", "").lower()
     from_start_df = df_from_start(start_f)
     start_stats = [from_start_df["tobs"].min(),from_start_df["tobs"].max(), from_start_df["tobs"].mean()]
-    # Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start or start-end range.
     return jsonify(start_stats)
 
 @app.route("/api/v1.0/<start>/<end>")
-def holup(start,end):
+def start_end(start,end):
     start_f = start.replace(" ", "").lower()
     end_f = end.replace(" ", "").lower()
+
     from_start_df = df_from_start(start_f)
     to_end_df = from_start_df[from_start_df["date"] <= end_f]
-    # create boolean mask for rows to be deleted
+
     mask = from_start_df['date'] < end_f
-    # drop rows from DataFrame using the boolean mask
     to_end_df = from_start_df.drop(from_start_df[mask].index, inplace=False)
     end_stats = [to_end_df["tobs"].min(),to_end_df["tobs"].max(), to_end_df["tobs"].mean()]
-    # Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start or start-end range.
     return jsonify(end_stats)
-
 
 if(__name__=="__main__"):
     app.run(debug=True)
-
-
